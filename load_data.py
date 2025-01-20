b4147ac0cca40bfc639b4da9f71bf7e6ea343e83
Tarek Al Mustafa <85288390+TarekAlMustafa@users.noreply.github.com>
2025-01-20 17:52:55 +0100

    #300

--- load_data.py
+++ load_data.py
@@ -1,12 +1,24 @@
 import os
+import json
+from tqdm import tqdm
 
 def load_abstracts():
     result = {}
-    for filename in os.listdir("unlabeled abstracts"):
+    for filename in tqdm(os.listdir("unlabeled abstracts")):
         with open(f"unlabeled abstracts/{filename}", "r") as f:
             text = f.read().strip()
             title, abstract, doi = text.split("\n")
 
             index = int(filename[:-4])
             result[index] = {"title": title, "abstract": abstract, "index": index}
+    return result
+
+def load_concepts():
+    result = {}
+    for filename in tqdm(os.listdir("detected_concepts")):
+        with open(f"detected_concepts/{filename}", "r") as f:
+            concepts = json.load(f)
+            
+            index = int(filename[:-5])
+            result[index] = concepts
     return result
